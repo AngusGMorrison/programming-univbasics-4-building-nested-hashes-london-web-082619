649526c2d1b9e00597cc7ea22555f92c15040a4d
Angus Morrison <contact@angus-morrison.com>
2019-08-07 12:47:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  { railroads: { pieces: 4 },
+  /*{ railroads: { pieces: 4 },
                { rent_in_dollars: {
                   one_piece_owned: 25,
                   two_pieces_owned: 50,
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
                   b_and_o_railroad: {},
                   shortline_railroad: {}
                   }
-               }
+               } */
 
 end
 
